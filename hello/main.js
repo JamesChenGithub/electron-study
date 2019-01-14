af1807a7bbd9bfd3d2282f5a9500c91a3ecdde31
alexichen <403725592@qq.com>
2019-01-14 23:12:41 +0800

    添加hello

---
+++ hello/main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+
+let mainWindow;
+
+const createWindow = ()=>{
+    mainWindow = new BrowserWindow({
+        width : 800,
+        height : 600
+    });
+
+    const indexpath = url.format({
+        pathname : path.join(__dirname, 'index.html'),
+        protocol : 'file:',
+        slashes : true
+    });
+
+    mainWindow.loadURL(indexpath);
+    mainWindow.webContents.openDevTools(false);
+
+    mainWindow.on('close', ()=>{
+        mainWindow = null;
+    });
+}
+app.on('ready', createWindow);
+
+app.on('windows-all-close', ()=>{
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', ()=>{
+    if(win === null) {
+        createWindow();
+    }
+});

386812fba8a6c32f892fade3b01def2c2426c6ab
alexichen <alexichen@tencent.com>
2019-01-15 12:22:13 +0800

    添加主进程渲染进程通信

--- hello/render-process/msg-a.js
+++ hello/render-process/msg-a.js
@@ -5,3 +5,17 @@ btn.onclick = ()=>{
     const msg = ipcRenderer.sendSync('msg-a');
     console.log(msg);
 };
+
+
+const btn2 = document.querySelector('#msg-a-1');
+btn2.onclick = ()=>{
+    const msg = ipcRenderer.send('msg-a-1', {name : 'hello'});
+    console.log(msg);
+    
+    ipcRenderer.on('msg-a-1-back', (event, args)=>{
+        console.log(event);
+        console.log(args);
+        console.log(args.name);
+    });
+};
+

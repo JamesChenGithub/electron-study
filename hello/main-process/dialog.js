35322f4055e64ee27a6dfaeb11ccae23e414f0ea
alexichen <alexichen@tencent.com>
2019-01-16 14:07:15 +0800

    添加dialog.js

--- hello/main-process/dialog.js
+++ hello/main-process/dialog.js
@@ -1,16 +1,30 @@
 const {dialog} = require('electron');
 
-dialog.showErrorBox('titile', 'error');
+// dialog.showErrorBox('titile', 'error');
 
-dialog.showMessageBox({
-    type : 'info',
-    title : 'message',
-    message : 'hello electron',
-    buttons : ['好的', '不好']
-}, (index)=>{
-    if (index == 0) {
-        console.log('好的');
-    } else {
-        console.log('错的');
-    }
+// dialog.showMessageBox({
+//     type : 'info',
+//     title : 'message',
+//     message : 'hello electron',
+//     buttons : ['好的', '不好']
+// }, (index)=>{
+//     if (index == 0) {
+//         console.log('好的');
+//     } else {
+//         console.log('错的');
+//     }
+// })
+
+
+dialog.showOpenDialog({
+    properties : ['openDirectory', 'openFile']
+}, (files)=>{
+    console.log(files);
+})
+
+dialog.showSaveDialog({
+    title : 'save something',
+    filters : [{name : 'some', extensions: ['js']}]
+}, (filename)=>{
+    console.log(filename);
 })
